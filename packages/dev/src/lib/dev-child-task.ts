26fc0f6a7aa5ee06361a21708c81b449d0408746
SalvatorePreviti <roorback@gmail.com>
2021-10-26 12:45:47 +0100

    dev-child-task supports array args

--- packages/dev/src/lib/dev-child-task.ts
+++ packages/dev/src/lib/dev-child-task.ts
@@ -4,21 +4,42 @@ import { devLog } from './dev-log'
 import type { DevLogTimeOptions } from './dev-log'
 import { resolveModuleBin } from '../modules/resolve'
 
-type DevChildTaskArg = string | null | undefined | number | false
+type DevChildTaskArg =
+  | string
+  | null
+  | undefined
+  | number
+  | false
+  | readonly (string | null | undefined | number | false)[]
 
-function getDevChildTaskArgs(args: readonly DevChildTaskArg[]) {
-  return args
-    .filter((arg) => arg !== null && arg !== undefined && arg !== false)
-    .map((arg) => (typeof arg !== 'string' ? `${arg}` : arg))
+function getDevChildTaskArgs(args: readonly DevChildTaskArg[]): string[] {
+  const result: string[] = []
+  const append = (array: readonly DevChildTaskArg[], level: number) => {
+    for (const arg of array) {
+      if (arg !== null && arg !== undefined && arg !== false) {
+        if (Array.isArray(arg)) {
+          if (level > 8) {
+            throw new Error('getDevChildTaskArgs array overflow')
+          }
+          append(arg, level + 1)
+        } else {
+          result.push(typeof arg !== 'string' ? `${arg}` : arg)
+        }
+      }
+    }
+  }
+  append(args, 0)
+  return result
 }
 
 export const devChildTask = {
   /** Spawn a new process, redirect stdio and await for completion. */
   async spawn(
     command: string,
-    args: readonly DevChildTaskArg[] = [],
+    inputArgs: readonly DevChildTaskArg[] = [],
     options?: child_process.SpawnOptions & { title?: string; showStack?: boolean } & DevLogTimeOptions
   ) {
+    const args = getDevChildTaskArgs(inputArgs)
     const cmd = [command, ...args].join(' ')
     const title = (options && options.title) || (cmd.length < 40 ? cmd : command)
     const exitError = new Error(`Child process "${title}" failed`)
@@ -27,7 +48,7 @@ export const devChildTask = {
     }
     const spawn = () =>
       _awaitChildProcess(
-        child_process.spawn(command, getDevChildTaskArgs(args), { env: process.env, stdio: 'inherit', ...options }),
+        child_process.spawn(command, args, { env: process.env, stdio: 'inherit', ...options }),
         cmd,
         exitError
       )
@@ -37,9 +58,10 @@ export const devChildTask = {
   /** Forks the node process that runs the given module, redirect stdio and await for completion. */
   async fork(
     moduleId: string,
-    args: readonly DevChildTaskArg[] = [],
+    inputArgs: readonly DevChildTaskArg[] = [],
     options?: child_process.ForkOptions & { title?: string; showStack?: boolean } & DevLogTimeOptions
   ) {
+    const args = getDevChildTaskArgs(inputArgs)
     const cmd = [moduleId, ...args].join(' ')
     const title = (options && options.title) || (cmd.length < 40 ? cmd : moduleId)
     const exitError = new Error(`Child process "${title}" failed`)
